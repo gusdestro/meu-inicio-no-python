43ed50b6d5206ee2b6f899ceedd99c5dc0019a6b
Gustavo Destro <dstrozn@gmail.com>
2025-11-01 14:32:37 +0000

    start comit

---
+++ main.py
@@ -0,0 +1,11 @@
+nome = 'Gustavo Destro' # string
+mensagem = 'Bom dia, tudo bem?' # string
+idade = 28 # integer
+altura = 1.81 # float
+professor = True # boolean
+
+print(type(nome))
+print(mensagem)
+print(idade)
+print(altura)
+print(professor)
